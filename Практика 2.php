49308dc9a3f14a2f6535806eee15e1f2e92ba144
arteizyranov-design <arteizyranov@gmail.com>
2026-09-25 14:00:49 +0300

    First Commit

---
+++ Практика 2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Формула 1</h1>
+   <?php
+    $a = 5;
+    $b = 4;
+    $c = 2;
+    $d = 3;
+
+
+    $result = (($a / $c) * ($b / $d)) - (($a * $b - $c) / ($c * $d));
+
+    echo "Результат вычисления: " . $result;
+    ?>
+    <h1> Формула 2</h1>
+   <?php
+    $x = 4;
+    $y = 2;
+
+    $result = ($x + $y) / ($y + 1) - ($x * $y - 12) / (34 + $x);
+
+    echo "Результат вычислений: " . $result;
+    ?>
+    <h1> Формула 3-4</h1>
+    <?php
+        $x = 4;
+$y = 2;
+
+$result = (($x + 1) / ($x - 1))**4 + 18 * $x * $y**2 / ((1 + 1 / $x**2)**5 - 12 * $x**2 * $y);
+
+echo "Результат вычислений: " . $result;
+?>
+
+</body>
+</html>
